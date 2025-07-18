8413bb618c6932772f55a99e4312f03fc42e80bb
Simon Gander <simon.gander@ioz.ch>
2025-07-18 09:52:34 +0200

    change recipe to 2eu

--- src/main/resources/kubejs/server_scripts/atmosphere_generator.js
+++ src/main/resources/kubejs/server_scripts/atmosphere_generator.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
   event.custom({
     type: "modern_industrialization:atmosphere_generator",
-    eu: 1,
+    eu: 2,
     duration: 1200,
     fluid_inputs: [{ fluid: "modern_industrialization:oxygen", amount: 1000 }],
     fluid_outputs: [{ fluid: "modern_industrialization:oxygen", amount: 0 }],
